2bc7094b11a91069df9b4b87b2f5bbf7356976d6
barbadeadesh <barbadeadesh2020@email.com>
2022-04-05 22:56:27 +0530

    ADDED: Functionality to check th first name

--- com.bridgelab.junit/src/com/bridgelab/junit/UserRegistrationCase.java
+++ com.bridgelab.junit/src/com/bridgelab/junit/UserRegistrationCase.java
@@ -1,6 +1,13 @@
 package com.bridgelab.junit;
 
+import java.util.regex.Pattern;
+
 public class UserRegistrationCase {
+	
+	public boolean validateFirstName(String str) {
+		boolean check = Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", str);
+		return check;
+	}
 
 	public static void main(String args[]) {
 
--- com.bridgelab.junit/src/com/bridgelab/junit/UserRegistrationTest.java
+++ com.bridgelab.junit/src/com/bridgelab/junit/UserRegistrationTest.java
@@ -1,5 +1,12 @@
 package com.bridgelab.junit;
-
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
 public class UserRegistrationTest {
 
+	@Test
+	public void givenfirstnameshouldbeTrue() {
+		UserRegistrationCase userRegistration = new UserRegistrationCase();
+		boolean result = userRegistration.validateFirstName("Adesh");
+		Assert.assertEquals(true, result);
+	}
 }
