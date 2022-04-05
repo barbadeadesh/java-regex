5b27ee983aacec97549e162461d41c04cde34ebd
barbadeadesh <barbadeadesh2020@email.com>
2022-04-05 22:04:38 +0530

    ADDED: functionality to create mobile number by using regex pattern

--- com.bridgelab.regex/src/com/bridgelab/regex/Regex.java
+++ com.bridgelab.regex/src/com/bridgelab/regex/Regex.java
@@ -45,28 +45,48 @@ public class Regex {
 			System.out.println("Last Name is Invalid");
 		}
 	}
+
 	/**
 	 * Create valid Mail method for getting hole mail Id By Using Expression
 	 */
-		public static void ValidEmail() {
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter an email");
-			String email = sc.nextLine();
-			System.out.println("Email is: " + email);
-			Pattern pattern = Pattern.compile("^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$");
-			Matcher matcher = pattern.matcher(email);
-			boolean MatchFound = matcher.matches();
-			if (MatchFound) {
-				System.out.println("Email is Valid");
-			} else
-				System.out.println("Email is Invalid");
+	public static void ValidEmail() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter an email");
+		String email = sc.nextLine();
+		System.out.println("Email is: " + email);
+		Pattern pattern = Pattern.compile("^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$");
+		Matcher matcher = pattern.matcher(email);
+		boolean MatchFound = matcher.matches();
+		if (MatchFound) {
+			System.out.println("Email is Valid");
+		} else
+			System.out.println("Email is Invalid");
+	}
+
+	/**
+	 * Create valid MobileNum method for verify mobile number By Using Expression
+	 */
+	public static void ValidMobileNum() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a mobile number: ");
+		String mob = sc.nextLine();
+		System.out.println("Mobile Number is: " + mob);
+		Pattern pattern = Pattern.compile("^([0-9]{2}) ([1-9]{1}[0-9]{9,})$");
+		Matcher matcher = pattern.matcher(mob);
+		boolean MatchFound = matcher.matches();
+		if (MatchFound) {
+			System.out.println("Mobile number is valid");
+		} else {
+			System.out.println("Mobile Number is Invalid");
 		}
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Wlcome To The Regex");
 		AddFirstName();
 		AddLastName();
 		ValidEmail();
+		ValidMobileNum();
 
 	}
 
