70d225d8673c5b018b761316381217c24f6f15a3
barbadeadesh <barbadeadesh2020@email.com>
2022-04-05 21:09:00 +0530

    Added: Functionality to add first name in regex

--- com.bridgelab.regex/src/com/bridgelab/regex/Start.java
+++ com.bridgelab.regex/src/com/bridgelab/regex/Start.java
@@ -1,10 +1,33 @@
 package com.bridgelab.regex;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Start {
+	
+	/**
+	 * Create Add First Name method for getting First Name By Using Expression
+	 */
+	public static void AddFirstName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a First Name:");
+		String name = sc.nextLine();
+		System.out.println("First Name is : " + name);
+		Pattern pattern = Pattern.compile("^([A-Z]{1})([a-z]{2,})$");
+		Matcher matcher = pattern.matcher(name);
+		boolean MatchFound = matcher.matches();
+
+		if (MatchFound) {
+			System.out.println("Name is Valid");
+		} else {
+			System.out.println("Name is invalid");
+		}
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Regex");
-
+		AddFirstName();
 	}
 
 }
